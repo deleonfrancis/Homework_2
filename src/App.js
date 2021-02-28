1d710c342a8efed18b20242929515d934785f81a
Deleon Francis <dxfrancis.coding@gmail.com>
2021-02-28 17:38:42 -0500

    hash instead of browser.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import "./css/portfolio.css";
 import "./css/styles.css";
 import Navbar from "./components/Navbar";
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -64,7 +64,7 @@ function Navbar() {
             <Link
               to="/"
               className={
-                // window.location.pathname === "/" ||
+                window.location.pathname === "/" ||
                 window.location.pathname === "/about"
                   ? "nav-link active"
                   : "nav-link"
