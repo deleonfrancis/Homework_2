133a9e32c2a10c87db8ae887f856803c119b3987
Deleon Francis <dxfrancis.coding@gmail.com>
2021-02-28 11:25:43 -0500

    changed class to className footer and navbar.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,23 +5,23 @@ function Footer() {
 const date = new Date();
 const year = date.getFullYear();
 
-  return (<footer class="footer mt-auto container-fluid">
-  <div class="row">
-    <div class="col bg-green p-1"></div>
+  return (<footer className="footer mt-auto container-fluid">
+  <div className="row">
+    <div className="col bg-green p-1"></div>
   </div>
-  <div class="row">
-      <div class="col-md-12 text-center">
+  <div className="row">
+      <div className="col-md-12 text-center">
         <a href="https://github.com/deleonfrancis" target="_blank" rel="noreferrer">
-          <i class="fab fa-github fa-3x text-white p-3"></i>
+          <i className="fab fa-github fa-3x text-white p-3"></i>
         </a>
         <a href="https://www.linkedin.com/in/deleon-francis-9b09291b9/" target="_blank" rel="noreferrer">
-          <i class="fab fa-linkedin fa-3x p-3"></i>
+          <i className="fab fa-linkedin fa-3x p-3"></i>
         </a>              
       </div>
-      <div class="col-md-12 text-center">
-        <p class="text-white m-0">E-mail: <span class="text-primary">dxfrancis.coding@gmail.com</span></p>
-        <p class="text-white">Phone: <span class="text-primary">(407) 437-1137</span></p>
-        <p class="copyright-text py-1">{`© Copyright ${year}`}</p>
+      <div className="col-md-12 text-center">
+        <p className="text-white m-0">E-mail: <span className="text-primary">dxfrancis.coding@gmail.com</span></p>
+        <p className="text-white">Phone: <span className="text-primary">(407) 437-1137</span></p>
+        <p className="copyright-text py-1">{`© Copyright ${year}`}</p>
       </div>
     </div>    
 </footer>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,27 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+// Depending on the current path, this component sets the "active" className on the appropriate navigation link item
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-white p-2">
-      <h1 class="py-2 ml-3 mb-0" id="my-name">Deleon Xavier Francis</h1>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-white p-2">
+      <h1 className="py-2 ml-3 mb-0" id="my-name">Deleon Xavier Francis</h1>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul class="navbar-nav link-text-size">
-            <li class="nav-item active">
-                <a class="nav-link" href="./resume.html">Resume</a>
+      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul className="navbar-nav link-text-size">
+            <li className="nav-item active">
+                <a className="nav-link" href="./resume.html">Resume</a>
             </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./contact.html">Contact</a>
+          <li className="nav-item">
+            <a className="nav-link" href="./contact.html">Contact</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./portfolio.html">Portfolio</a>
+            <li className="nav-item">
+              <a className="nav-link" href="./portfolio.html">Portfolio</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../index.html">About</a>
+            <li className="nav-item">
+              <a className="nav-link" href="../index.html">About</a>
             </li>
           </ul>
         </div>
