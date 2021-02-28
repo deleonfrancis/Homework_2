77074be1fc6045d5760a2bf6a799035cdf48b9c1
Deleon Francis <dxfrancis.coding@gmail.com>
2021-02-28 12:50:26 -0500

    added html to Home page.

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Wrapper(props) {
-  return <main className="wrapper flex-fill p-3 py-5 mt-1">{props.children}</main>;
+  return <main className="wrapper">{props.children}</main>;
 }
 
 export default Wrapper;
