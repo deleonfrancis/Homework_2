85c5464b5268ad82c3b9954976061097f46a730f
Deleon Francis <dxfrancis.coding@gmail.com>
2021-02-28 12:12:24 -0500

    active class in nav links now works.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,27 +5,77 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white p-2">
-      <h1 className="py-2 ml-3 mb-0" id="my-name">Deleon Xavier Francis</h1>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <h1 className="py-2 ml-3 mb-0" id="my-name">
+        Deleon Xavier Francis
+      </h1>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <div
+        className="collapse navbar-collapse justify-content-end"
+        id="navbarNav"
+      >
         <ul className="navbar-nav link-text-size">
-            <li className="nav-item active">
-                <a className="nav-link" href="/resume">Resume</a>
-            </li>
           <li className="nav-item">
-            <a className="nav-link" href="/contact">Contact</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/portfolio">Portfolio</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">About</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+            <Link
+              to="/resume"
+              className={
+                window.location.pathname === "/resume"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Resume
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact"
+              className={
+                window.location.pathname === "/contact"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Contact
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/portfolio"
+              className={
+                window.location.pathname === "/portfolio"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                // window.location.pathname === "/" ||
+                window.location.pathname === "/about"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
