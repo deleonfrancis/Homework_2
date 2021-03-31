55c5e755bd537966f4d460cddf19e05d3689780d
Deleon Francis <dxfrancis.coding@gmail.com>
2021-03-31 16:19:13 -0400

    started styling profolio page. needs projects.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,81 +1,70 @@
-import React from "react";
-import Project from "../components/Project";
-import mood_1 from "../images/gifs/mood_1.gif";
-import weatherdash1 from "../images/gifs/weatherdash1.gif";
-import dayGif from "../images/gifs/dayGif.gif";
-import burgers from "../images/gifs/burgers.gif";
-import ed_screenshot from "../images/screenshots/ed_screenshot.png";
-import donde_login from "../images/screenshots/donde_login.png"
+import React, { Fragment } from "react";
+import Navbar from "../components/Navbar";
+// import Project from "../components/Project";
 
 function Portfolio() {
   return (
-    <section className="p-3 py-5 mt-1">
-      <div className="bg-white pb-5 center-this col-lg-8 col-md-12 col-sm-12 col-xs-12 shadow-lg">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-lg-6">
-              <h1 className="font-weight-bold text-green pt-3 mb-0">
-                Portfolio
-              </h1>
+    <Fragment>
+      <Navbar />
+      <section id="portfolio-section" className="p-3 py-5 mt-1 bg-light">
+        <h1 className="pt-3 mb-0">Portfolio</h1>
+        <hr />
+        Projects coming soon...
+
+            {/* <div className="row center-this d-flex justify-content-between">
+              Project 1
+              <Project
+                name="Dondé"
+                src={donde_login}
+                repo="https://github.com/jmasone15/travel-planner-project"
+                website="https://shielded-woodland-30004.herokuapp.com/"
+              />
+
+              Project 2
+              <Project
+                name="Weather Dashboard"
+                src={weatherdash1}
+                repo="https://github.com/deleonfrancis/Weather_Dashboard"
+                website="https://deleonfrancis.github.io/Weather_Dashboard/"
+              />
             </div>
-          </div>
-          <hr />
 
-          <div className="row center-this d-flex justify-content-between">
-            {/* Project 1 */}
-            <Project
-              name='Dondé'
-              src={donde_login}
-              repo="https://github.com/jmasone15/travel-planner-project"
-              website="https://shielded-woodland-30004.herokuapp.com/"
-            />
+            <div className="row center-this d-flex justify-content-between">
+              Project 3
+              <Project
+                name="Work Day Scheduler"
+                src={dayGif}
+                repo="https://github.com/deleonfrancis/Work_Day_Scheduler"
+                website="https://deleonfrancis.github.io/Work_Day_Scheduler/"
+              />
 
-            {/* Project 2 */}
-            <Project
-              name="Weather Dashboard"
-              src={weatherdash1}
-              repo="https://github.com/deleonfrancis/Weather_Dashboard"
-              website="https://deleonfrancis.github.io/Weather_Dashboard/"
-            />
-          </div>
+              Project 4
+              <Project
+                name="Burger Logger"
+                src={burgers}
+                repo="https://github.com/deleonfrancis/Burgers_logger"
+                website="https://evening-river-12924.herokuapp.com/"
+              />
+            </div>
 
-          <div className="row center-this d-flex justify-content-between">
-            {/* Project 3 */}
-            <Project
-              name="Work Day Scheduler"
-              src={dayGif}
-              repo="https://github.com/deleonfrancis/Work_Day_Scheduler"
-              website="https://deleonfrancis.github.io/Work_Day_Scheduler/"
-            />
-
-            {/* Project 4 */}
-            <Project
-              name="Burger Logger"
-              src={burgers}
-              repo="https://github.com/deleonfrancis/Burgers_logger"
-              website="https://evening-river-12924.herokuapp.com/"
-            />
-          </div>
-
-          <div className="row center-this d-flex justify-content-between">
-            {/* Project 5 */}
-            <Project
-              name="Employee Directory"
-              src={ed_screenshot}
-              repo="https://github.com/deleonfrancis/Employee_Directory"
-              website="https://deleonfrancis.github.io/Employee_Directory/"
-            />
-            {/* Project 6 */}
-            <Project
-              name='Group Project "Mood"'
-              src={mood_1}
-              repo="https://github.com/Bokosyk/Mood"
-              website="https://bokosyk.github.io/Mood/"
-            />
-          </div>
-        </div>
-      </div>
-    </section>
+            <div className="row center-this d-flex justify-content-between">
+              Project 5
+              <Project
+                name="Employee Directory"
+                src={ed_screenshot}
+                repo="https://github.com/deleonfrancis/Employee_Directory"
+                website="https://deleonfrancis.github.io/Employee_Directory/"
+              />
+              Project 6
+              <Project
+                name='Group Project "Mood"'
+                src={mood_1}
+                repo="https://github.com/Bokosyk/Mood"
+                website="https://bokosyk.github.io/Mood/"
+              />
+            </div> */}
+      </section>
+    </Fragment>
   );
 }
 
