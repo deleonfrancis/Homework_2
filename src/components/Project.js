0bb0484eb4d7d147f4fdb44d39706376b2ba5a00
Deleon Francis <dxfrancis.coding@gmail.com>
2021-04-04 16:56:18 -0400

    added details icon to projects.

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,39 +5,60 @@ function Project(props) {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-      <div
-        style={{ width: "400px", height: "205px", margin: "10px" }}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        className=""
-      >
-        {!isHovered && (
+    <div
+      style={{ width: "400px", height: "205px", margin: "10px", position: "relative" }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className=""
+    >
+      {!isHovered && (
+        <div style={{position: "absolute"}}>
           <img
             src={props.src}
             alt={`${props.name} pic`}
             className="image-fluid port-image"
-            style={{}}
           />
-        )}
-        {isHovered && (<FadeIn>
+          <div className="d-flex justify-content-end">
+          <i className="fas fa-info-circle port-icon text-dark"></i>
+          </div>
+          
+        </div>
+      )}
+      {isHovered && (
+        <FadeIn>
           <div className="">
-          <h3 className="project-title">{props.name}</h3>
-          <p className="project-detail">{props.detail}</p>
-            <div className="btn-block" style={{width:"75%", margin: "0 auto 5px"}}>
-              <a href={props.repo} target="_blank" rel="noreferrer" className="to-site-btn btn-block text-bg">
+            <h3 className="project-title">{props.name}</h3>
+            <p className="project-detail">{props.detail}</p>
+            <div
+              className="btn-block"
+              style={{ width: "75%", margin: "0 auto 5px" }}
+            >
+              <a
+                href={props.repo}
+                target="_blank"
+                rel="noreferrer"
+                className="to-site-btn btn-block text-bg"
+              >
                 Repository
               </a>
-              </div>
-            <div className="btn-block" style={{width:"75%", margin: "0 auto 5px"}}>
-              <a href={props.website} target="_blank" rel="noreferrer" className="to-site-btn btn-block text-bg">
+            </div>
+            <div
+              className="btn-block"
+              style={{ width: "75%", margin: "0 auto 5px" }}
+            >
+              <a
+                href={props.website}
+                target="_blank"
+                rel="noreferrer"
+                className="to-site-btn btn-block text-bg"
+              >
                 Website
               </a>
             </div>
           </div>
         </FadeIn>
-          
-        )}
-      </div>
+      )}
+    </div>
   );
 }
 
