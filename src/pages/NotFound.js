37a02423b08cbe7d83f27459412bd6c99560a3cc
Deleon Francis <dxfrancis.coding@gmail.com>
2021-04-11 12:51:24 -0400

    added links in page not found.

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FadeIn from "../components/FadeIn";
+import LinkButtons from "../components/LinkButtons";
 
 const NotFound = () => {
   return (
@@ -7,6 +8,7 @@ const NotFound = () => {
       <section className="flex-fill p-3 py-5 bg-light shadow-lg" style={{width:"90%", margin: " 20px auto"}}>
         <h1>Page Not Found</h1>
         <hr style={{ width: "100%", margin:"0 0 10px 0"}}/>
+        <LinkButtons about="px-2 text-link" portfolio="px-2 text-link" resume="px-2 text-link" />
         This page does not exist.
       </section>
     </FadeIn>
