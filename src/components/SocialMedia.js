fe51fbe5942227546fad62dce63e9d7df4caf3af
Deleon Francis <dxfrancis.coding@gmail.com>
2021-04-11 22:08:51 -0400

    social media center fix.

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom'
 
 function SocialMedia() {
     return (
-        <div className="row d-flex justify-content-center" style={{margin:"10px auto"}}>
-            <div className="col-sm-3 text-center mb-2">
+        <div className="row d-flex justify-content-center" //style={{margin:"10px auto"}}
+        >
+            <div className="col-sm-6 col-md-4 text-center mb-2">
                 <Link to="https://github.com/deleonfrancis" target="_blank" rel="noreferrer" className="link-detail">
                     <i className="fab fa-github fa-2x text-dark px-3"></i> <br/> Github
                 </Link>              
             </div>
-            <div className="col-sm-3 text-center">
+            <div className="col-sm-6 col-md-4 text-center">
                 <Link to="https://www.linkedin.com/in/deleon-francis-9b09291b9/" target="_blank" rel="noreferrer" className="link-detail">
                     <i className="fab fa-linkedin fa-2x px-3 text-primary"></i> <br/> LinkedIn
                 </Link>              
