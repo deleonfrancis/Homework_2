83a8bf58df75faf5d755b9fdb837de5ce1744df0
Deleon Francis <dxfrancis.coding@gmail.com>
2021-04-20 20:01:48 -0400

    fixed links to social media.

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 function SocialMedia() {
     return (
         <div className="row d-flex justify-content-center"
         >
             <div className="col-sm-6 col-md-4 text-center mb-2">
-                <Link to="https://github.com/deleonfrancis" target="_blank" rel="noreferrer" className="link-detail">
+                <a href="https://github.com/deleonfrancis" target="_blank" rel="noreferrer" className="link-detail">
                     <i className="fab fa-github fa-2x text-dark px-3"></i> <br/> Github
-                </Link>              
+                </a>              
             </div>
             <div className="col-sm-6 col-md-4 text-center">
-                <Link to="https://www.linkedin.com/in/deleon-francis-9b09291b9/" target="_blank" rel="noreferrer" className="link-detail">
+                <a href="https://www.linkedin.com/in/deleon-francis-9b09291b9/" target="_blank" rel="noreferrer" className="link-detail">
                     <i className="fab fa-linkedin fa-2x px-3 text-primary"></i> <br/> LinkedIn
-                </Link>              
+                </a>              
             </div>
       </div>
     )
