8666007d0cc324128df678563d2415bee2c89509
Deleon Francis <dxfrancis.coding@gmail.com>
2021-06-03 16:28:41 -0400

    restyled and changed the words on about section.

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,13 +7,22 @@ import FadeIn from "../components/FadeIn";
 import HamburgerBtn from "../components/HamburgerBtn";
 import SkillsIcons from "../components/SkillsIcons";
 
-function About({isLargerThanIPad, isSmallerThanIPad }) {
+function About({ isLargerThanIPad, isSmallerThanIPad }) {
   return (
     <FadeIn>
-      <section id="about-section" className="flex-fill p-3 py-5 bg-light shadow-lg">
+      <section
+        id="about-section"
+        className="flex-fill p-3 py-5 bg-light shadow-lg"
+      >
         <h1 className="mb-0 rale-text">About</h1>
         <hr />
-        {isLargerThanIPad && <LinkButtons about="px-2 text-link-active" portfolio="px-2 text-link" resume="px-2 text-link" />}
+        {isLargerThanIPad && (
+          <LinkButtons
+            about="px-2 text-link-active"
+            portfolio="px-2 text-link"
+            resume="px-2 text-link"
+          />
+        )}
         {isSmallerThanIPad && <HamburgerBtn />}
         <div className="row">
           <div className="col-lg-6">
@@ -22,28 +31,36 @@ function About({isLargerThanIPad, isSmallerThanIPad }) {
                 className="img-fluid"
                 src={xavier_picture}
                 alt="Photograph of Xavier"
-                style={{width:"150px", borderRadius: "10px"}}
+                style={{ width: "150px", borderRadius: "10px" }}
               />
-              <br/>
+              <br />
               <h2 className="text-bg my-3 about-label">About Me</h2>
               <div style={{ width: "60%", margin: "auto auto 20px" }}>
-                <p className="about-detail">
-                  I'm a detail-oriented, diligent, and self-motivated Full Stack Web Developer based
-                  in Orlando, Florida.
-                </p>
-                <p className="about-detail">
-                  Let's create something breathtaking together.
+                <p className="about-detail mt-3">
+                  I'm a passionate, solutions-oriented professional with a proven
+                  background in designing and building web applications.
+                  Adept in utilizing JavaScript, React, and a wide range of
+                  other technologies during application development process.
                 </p>
                 <SocialMedia />
-                <h3 className="text-dark m-0 email-label">E-mail: <span style={{wordWrap: "break-word"}}> <a href="mailto:dxfrancis.coding@gmail.com" className="text-email">dxfrancis.coding@gmail.com</a></span></h3>
-                
+                <h3 className="text-dark mt-4 email-label">
+                  E-mail:{" "}
+                  <span style={{ wordWrap: "break-word" }}>
+                    {" "}
+                    <a
+                      href="mailto:dxfrancis.coding@gmail.com"
+                      className="text-email"
+                    >
+                      dxfrancis.coding@gmail.com
+                    </a>
+                  </span>
+                </h3>
               </div>
             </div>
           </div>
           <div className="col-lg-5">
-          {isLargerThanIPad && <HorizontalBarChart /> }
-          {isSmallerThanIPad && <SkillsIcons />}
-            
+            {isLargerThanIPad && <HorizontalBarChart />}
+            {isSmallerThanIPad && <SkillsIcons />}
           </div>
         </div>
       </section>
